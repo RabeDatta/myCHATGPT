b14cc243b6f14d678715ef92d6fbcbaa90e223d5
RabeDatta <robedutt@gmail.com>
2023-04-29 12:38:31 -0400

    jwt auth

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,4 +1,3 @@
-/* 
 import { api } from "@/api/apiInstances";
 import { createContext, useContext, useEffect, useState } from "react";
 import { contextType, currentUserType } from "./contextType";
@@ -42,74 +41,6 @@ export const AuthContextProvider = ({
 
   return (
     <AuthContext.Provider value={{ currentUser, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export const AuthState = () => {
-  return useContext(AuthContext);
-};
-*/
-
-import { api } from "@/api/apiInstances";
-import { createContext, useContext, useEffect, useState } from "react";
-import { contextType, currentUserType } from "./contextType";
-import { useNavigate } from "react-router-dom";
-
-export const AuthContext = createContext({} as contextType);
-
-export const AuthContextProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const [currentUser, setCurrentUser] = useState<currentUserType | null>(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const navigate = useNavigate();
-
-  console.log("currentUser", currentUser);
-
-  const login = async (loginData: { email: string; password: string }) => {
-    const { data } = await api.post("/auth/login", loginData);
-    setCurrentUser(data.user);
-    setIsAuthenticated(true);
-    return data.message;
-  };
-
-  const logout = async () => {
-    await api.post("/auth/logout");
-    setCurrentUser(null);
-    setIsAuthenticated(false);
-    navigate("/");
-  };
-
-  useEffect(() => {
-    const checkAuthStatus = async () => {
-      try {
-        const response = await api.get("/auth/auth-status", {
-          withCredentials: true,
-        });
-
-        if (response.status === 200) {
-          setCurrentUser(response.data.user);
-          setIsAuthenticated(true);
-        }
-      } catch (error) {
-        setCurrentUser(null);
-        setIsAuthenticated(false);
-        console.log(error);
-      }
-    };
-
-    checkAuthStatus();
-  }, []); // Keep the empty array to run the effect only once when the component mounts
-
-  return (
-    <AuthContext.Provider
-      value={{ currentUser, isAuthenticated, login, logout }}
-    >
       {children}
     </AuthContext.Provider>
   );
